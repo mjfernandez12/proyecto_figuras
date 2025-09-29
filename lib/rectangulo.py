e52a04bfb4006f2662b324ba7bf5b206c2b74139
Maria Juiz <maria.juiz@rai.usc.es>
2025-09-29 19:17:57 +0200

    Agregadas funcionalidades para el rectangulo

---
+++ lib/rectangulo.py
@@ -0,0 +1,8 @@
+def get_identificador() -> str:
+	return "rectangulo"
+def get_area(lado_1:float,lado_2:float)->float:
+	area=lado_1 * lado_2
+	return area
+def get_perimetro(lado_1:float,lado_2:float)->int:
+	perimetro= lado_1*2 + lado_2*2
+	return perimetro 
