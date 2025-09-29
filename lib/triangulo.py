6567cbe433d62fb72f8678bcd753ab6d63f12a80
Maria Juiz <maria.juiz@rai.usc.es>
2025-09-29 19:54:27 +0200

    Agregadas funcionalidades para el triangulo

--- lib/triangulo.py
+++ lib/triangulo.py
@@ -1,9 +1,9 @@
-def get_identificador() -> str
+def get_identificador() -> str:
 	return "triangulo"
-def get_area(base:int,altura:int)->float
+def get_area(base:int,altura:int)->float:
 	area=base(altura/2)
 	return area
-def get_perimetro(lado_a:int,lado_b:int,lado_c:int)->int
+def get_perimetro(lado_a:int,lado_b:int,lado_c:int)->int:
 	perimetro= lado_a +lado_b +lado_c
 	return perimetro 
 
