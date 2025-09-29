d5b97189497985c65496272542f51a20643b9eb8
Maria Juiz <maria.juiz@rai.usc.es>
2025-09-29 18:41:35 +0200

    Creadas las funciones del triangulo

---
+++ lib/triangulo.py
@@ -0,0 +1,9 @@
+def get_identificador() -> str
+	return "triangulo"
+def get_area(base:int,altura:int)->float
+	area=base(altura/2)
+	return area
+def get_perimetro(lado_a:int,lado_b:int,lado_c:int)->int
+	perimetro= lado_a +lado_b +lado_c
+	return perimetro 
+
